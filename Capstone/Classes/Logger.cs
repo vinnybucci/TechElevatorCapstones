4924848d0a4483e59a5d0ad099f4441199f81219
Vinny Bucci <vinnybucci10@gmail.com>
2020-10-08 11:39:03 -0400

    Added classes and unit tests all blank

---
+++ Capstone/Classes/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+    public class Logger
+    {
+        //add system.io, create log.txt datetime, method, action(balance/feed money/selection(product name, product location)), amount entered +- balance
+
+
+
+
+    }
+}
---
+++ Capstone/Classes/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.Classes
+{
+   public class Menu
+    {
+        //console.writelines: of display vending machine items, purchase, exit, 
+        //feed money, selct product, finish transaction, sales report. 
+
+
+
+
+    }
+}
