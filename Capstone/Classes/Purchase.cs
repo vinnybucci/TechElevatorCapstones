aafc1fe67a82ce62d2febdba3a71a75a518aa6ea
Chad Charlesworth <chadwulf516@gmail.com>
2020-10-09 11:39:38 -0400

    Finish Transaction Complete

--- Capstone/Classes/Purchase.cs
+++ Capstone/Classes/Purchase.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Capstone.Exceptions;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.Design;
+using System.IO;
 using System.Runtime.CompilerServices;
 using System.Security.Cryptography;
 using System.Text;
@@ -20,12 +22,58 @@ namespace Capstone.Classes
         }
 
 
+        public void Display()
+        {
+            string filePath = @"C:\Users\Student\workspace\Capstones\csharp-capstone-module-1-team-3\vendingmachine.csv";
+            
 
+            try
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    Console.Clear();
+                    if (Products.Count == 16)
+                    {
+
+                        foreach (Product product in Products)
+                        {
+                            if (product.Stock == 0)
+                            {
+                                Console.WriteLine($"{product.SlotLocation} {product.ProductName} {product.Price} quantity remaining: SOLD OUT");
+                            } else
+                            Console.WriteLine($"{product.SlotLocation} {product.ProductName} {product.Price} quantity remaining: {product.Stock}");
+
+                        }
+                    }
+                    else if (Products.Count == 0)
+                    {
+                        while (!sr.EndOfStream)
+                        {
+
+                            string line = sr.ReadLine();
+                            string[] productDetails = line.Split('|');
+                            Product product = new Product(productDetails[0], productDetails[1], productDetails[2], productDetails[3]);
+                            Products.Add(product);
+                        }
+
+                        
+                        foreach (Product product in Products)
+                        {
+                            Console.WriteLine($"{product.SlotLocation} {product.ProductName} {product.Price} quantity remaining: {product.Stock}");
+                        }
+                    }
+                }
+                    
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
         public void PurchaseMenu()
         {
 
             // do
-
             Console.WriteLine();
             Console.WriteLine("1. Feed Money");
 
@@ -49,7 +97,7 @@ namespace Capstone.Classes
                 else if (selection == "3")
                 {
 
-                    //FinalBalance()
+                    FinishTransaction();
                     //shouldContinue = false;
                 }
             }
@@ -91,7 +139,7 @@ namespace Capstone.Classes
             bool itemSelected = false;
             try
             {
-                while (!itemSelected)
+                do
                 {
                     switch (selection)
                     {
@@ -99,144 +147,284 @@ namespace Capstone.Classes
                             if (Products[0].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[0].Price);
-                                Console.WriteLine($"{Products[0].ProductName} {Products[0].Price} {Balance}");
+                                Console.WriteLine($"{Products[0].ProductName} {Products[0].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Crunch Crunch, Yum!");
                                 itemSelected = true;
-                            }                          
+                                Products[0].Stock -= 1;
+                            }else if(Products[0].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
+                            }
                             break;
                         case "A2":
                             if (Products[1].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[1].Price);
-                                Console.WriteLine($"{Products[1].ProductName} {Products[1].Price} {Balance}");
+                                Console.WriteLine($"{Products[1].ProductName} {Products[1].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Crunch Crunch, Yum!");
+                                itemSelected = true;
+                                Products[1].Stock -= 1;
+                            }
+                            else if (Products[1].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "A3":
                             if (Products[2].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[2].Price);
-                                Console.WriteLine($"{Products[2].ProductName} {Products[2].Price} {Balance}");
+                                Console.WriteLine($"{Products[2].ProductName} {Products[2].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Crunch Crunch, Yum!");
+                                itemSelected = true;
+                                Products[2].Stock -= 1;
+                            }
+                            else if (Products[2].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "A4":
                             if (Products[3].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[3].Price);
-                                Console.WriteLine($"{Products[3].ProductName} {Products[3].Price} {Balance}");
+                                Console.WriteLine($"{Products[3].ProductName} {Products[3].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Crunch Crunch, Yum!");
+                                itemSelected = true;
+                                Products[3].Stock -= 1;
+                            }
+                            else if (Products[3].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "B1":
                             if (Products[4].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[4].Price);
-                                Console.WriteLine($"{Products[4].ProductName} {Products[4].Price} {Balance}");
+                                Console.WriteLine($"{Products[4].ProductName} {Products[4].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Munch Munch, Yum!");
+                                itemSelected = true;
+                                Products[4].Stock -= 1;
+                            }
+                            else if (Products[4].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "B2":
                             if (Products[5].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[5].Price);
-                                Console.WriteLine($"{Products[5].ProductName} {Products[5].Price} {Balance}");
+                                Console.WriteLine($"{Products[5].ProductName} {Products[5].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Munch Munch, Yum!");
+                                itemSelected = true;
+                                Products[5].Stock -= 1;
+                            }
+                            else if (Products[5].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "B3":
                             if (Products[6].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[6].Price);
-                                Console.WriteLine($"{Products[6].ProductName} {Products[6].Price} {Balance}");
+                                Console.WriteLine($"{Products[6].ProductName} {Products[6].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Munch Munch, Yum!");
+                                itemSelected = true;
+                                Products[6].Stock -= 1;
+                            }
+                            else if (Products[6].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "B4":
                             if (Products[7].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[7].Price);
-                                Console.WriteLine($"{Products[7].ProductName} {Products[7].Price} {Balance}");
+                                Console.WriteLine($"{Products[7].ProductName} {Products[7].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Munch Munch, Yum!");
+                                itemSelected = true;
+                                Products[7].Stock -= 1;
+                            }
+                            else if (Products[7].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "C1":
                             if (Products[8].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[8].Price);
-                                Console.WriteLine($"{Products[8].ProductName} {Products[8].Price} {Balance}");
+                                Console.WriteLine($"{Products[8].ProductName} {Products[8].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Glug Glug, Yum!");
+                                itemSelected = true;
+                                Products[8].Stock -= 1;
+                            }
+                            else if (Products[8].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "C2":
                             if (Products[9].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[9].Price);
-                                Console.WriteLine($"{Products[9].ProductName} {Products[9].Price} {Balance}");
+                                Console.WriteLine($"{Products[9].ProductName} {Products[9].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Glug Glug, Yum!");
+                                itemSelected = true;
+                                Products[9].Stock -= 1;
+                            }
+                            else if (Products[9].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "C3":
                             if (Products[10].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[10].Price);
-                                Console.WriteLine($"{Products[10].ProductName} {Products[10].Price} {Balance}");
+                                Console.WriteLine($"{Products[10].ProductName} {Products[10].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Glug Glug, Yum!");
+                                itemSelected = true;
+                                Products[10].Stock -= 1;
+                            }
+                            else if (Products[10].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "C4":
                             if (Products[11].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[11].Price);
-                                Console.WriteLine($"{Products[11].ProductName} {Products[11].Price} {Balance}");
+                                Console.WriteLine($"{Products[11].ProductName} {Products[11].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Glug Glug, Yum!");
+                                itemSelected = true;
+                                Products[11].Stock -= 1;
+                            }
+                            else if (Products[11].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "D1":
                             if (Products[12].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[12].Price);
-                                Console.WriteLine($"{Products[12].ProductName} {Products[12].Price} {Balance}");
+                                Console.WriteLine($"{Products[12].ProductName} {Products[12].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Chew Chew, Yum!");
+                                itemSelected = true;
+                                Products[12].Stock -= 1;
+                            }
+                            else if (Products[12].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "D2":
                             if (Products[13].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[13].Price);
-                                Console.WriteLine($"{Products[13].ProductName} {Products[13].Price} {Balance}");
+                                Console.WriteLine($"{Products[13].ProductName} {Products[13].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Chew Chew, Yum!");
+                                itemSelected = true;
+                                Products[13].Stock -= 1;
+                            }
+                            else if (Products[13].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "D3":
                             if (Products[14].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[14].Price);
-                                Console.WriteLine($"{Products[14].ProductName} {Products[14].Price} {Balance}");
+                                Console.WriteLine($"{Products[14].ProductName} {Products[14].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Chew Chew, Yum!");
+                                itemSelected = true;
+                                Products[14].Stock -= 1;
+                            }
+                            else if (Products[14].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
                         case "D4":
                             if (Products[15].Stock > 0)
                             {
                                 Balance -= double.Parse(Products[15].Price);
-                                Console.WriteLine($"{Products[15].ProductName} {Products[15].Price} {Balance}");
+                                Console.WriteLine($"{Products[15].ProductName} {Products[15].Price:C2} {Balance:C2}");
                                 Console.WriteLine("Chew Chew, Yum!");
+                                itemSelected = true;
+                                Products[15].Stock -= 1;
+                            }
+                            else if (Products[15].Stock == 0)
+                            {
+                                throw new OutOfStockException("Item is Sold Out", 0);
                             }
                             break;
-                            
+                        default:
+                            throw new Exception("Enter a valid input");
+
                     }
-                }
+                } while (!itemSelected);
+                
                 PurchaseMenu();
+        }
+            catch(OutOfStockException oe)
+            {
+                Console.WriteLine(oe.Message);
+                PurchaseMenu();
+    }
+            catch(Exception e)
+            {
+                Console.WriteLine("The code you entered is invalid.");
+                PurchaseMenu();
+}
+            
+
+        }
+        public void FinishTransaction()
+        {
+            double quarter = 0.25;
+            double dime = 0.10;
+            double nickel = 0.05;
+            Dictionary<double, int> change = new Dictionary<double, int>()
+            {
+                {quarter, 0 },
+                {dime, 0 },
+                {nickel,0 },
+            };
+            try
+            {
+                do
+                {
+                    if (Balance * 100 % 25 == 0)
+                    {
+                        Balance -= quarter;
+                        change[quarter]++;
+                    }
+                    else if (Balance * 100 % 10 == 0)
+                    {
+                        Balance -= dime;
+                        change[dime]++;
+                    }
+                    else if (Balance * 100 % 5 == 0)
+                    {
+                        Balance -= nickel;
+                        change[nickel]++;
+                    }
+                } while (Balance > 0);
+                Console.WriteLine($"Your change is {change[quarter]} Quarters, {change[dime]} Dimes, and {change[nickel]} Nickels.");
             }
-            catch (Exception e)
+            catch(Exception e)
             {
 
             }
-
-
         }
-
 
     }
 }
--- Capstone/Classes/UI.cs
+++ Capstone/Classes/UI.cs
@@ -25,7 +25,7 @@ namespace Capstone.Classes
                     Purchase purchase = new Purchase(0);
                     if (selection == "1")
                     {
-                        Display();
+                        purchase.Display();
                         shouldContinue = true;
                     }
                     else if (selection == "2")
@@ -48,35 +48,7 @@ namespace Capstone.Classes
                 }
             } while (shouldContinue);
         }
-        public void Display()
-        {
-            string filePath = @"C:\Users\Student\workspace\Capstones\csharp-capstone-module-1-team-3\vendingmachine.csv";
-            List<Product> Products = new List<Product>();
-
-            try
-            {
-                using (StreamReader sr = new StreamReader(filePath))
-                {
-                    while (!sr.EndOfStream)
-                    {
-
-                        string line = sr.ReadLine();
-                        string[] productDetails = line.Split('|');
-                        Product product = new Product(productDetails[0], productDetails[1], productDetails[2], productDetails[3]);
-                        Products.Add(product);
-                    }
-                }
-                Console.Clear();
-                foreach (Product product in Products)
-                {
-                    Console.WriteLine($"{product.SlotLocation} {product.ProductName} {product.Price} quantity remaining: {product.Stock}");
-                }
-            }
-            catch (Exception e)
-            {
-
-            }
-        }
+        
         //console.writelines: of display vending machine items, purchase, exit, 
         //feed money, selct product, finish transaction, sales report. 
 
